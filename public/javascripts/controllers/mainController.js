bf7ab2470e75fbe143f5b4630ea8268bb45bbd0c
Cody Patterson <copatterson@csumb.edu>
2017-08-04 19:37:28 -0700

    ngsrc images and styling

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -1,3 +1,19 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = 'Portfolio';
+
+    $scope.decor = {
+        tri : {
+            left: {
+                path: 'images/left_tri_dec.png',
+                alt: 'left side triangles',
+                classDef: 'dec leftDec'
+            },
+            right: {
+                path: 'images/right_tri_dec.png',
+                alt: 'right side triangles',
+                classDef: 'dec rightDec'
+            }
+        }
+    };
+
 }]);
